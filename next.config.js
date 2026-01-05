2dcf3d0d1b2650fe7ed2c58e80726a1020cfd32c
dan.abbott <127796046+dan-abbott@users.noreply.github.com>
2026-01-05 15:45:54 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,  // Add this temporarily
+  productionBrowserSourceMaps: true,
   experimental: {
     serverActions: {
       bodySizeLimit: '50mb',
