522285fe08ee93835c80c659e12f93356ce0e345
abbottdan <abbott.dan@gmail.com>
2026-01-28 15:16:43 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-const { withSentryConfig } = require("@sentry/nextjs");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,  // Add this temporarily
@@ -23,41 +21,37 @@ const nextConfig = {
   },
 }
 
-// Sentry configuration options
-const sentryWebpackPluginOptions = {
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options
+// Check if we should use Sentry config wrapper
+// Only wrap with Sentry if the package is actually installed
+let finalConfig = nextConfig;
 
-  // Suppresses source map uploading logs during build
-  silent: true,
-  org: process.env.SENTRY_ORG,
-  project: process.env.SENTRY_PROJECT,
+try {
+  // Try to load Sentry - this will fail if package not installed
+  const { withSentryConfig } = require("@sentry/nextjs");
   
-  // Only upload source maps if auth token is provided
-  authToken: process.env.SENTRY_AUTH_TOKEN,
+  // Sentry configuration options
+  const sentryWebpackPluginOptions = {
+    silent: true,
+    org: process.env.SENTRY_ORG,
+    project: process.env.SENTRY_PROJECT,
+    authToken: process.env.SENTRY_AUTH_TOKEN,
+  }
+
+  const sentryOptions = {
+    widenClientFileUpload: true,
+    transpileClientSDK: true,
+    tunnelRoute: "/monitoring",
+    hideSourceMaps: true,
+    disableLogger: true,
+    automaticVercelMonitors: true,
+  }
+  
+  // Wrap config with Sentry if package exists
+  finalConfig = withSentryConfig(nextConfig, sentryWebpackPluginOptions, sentryOptions);
+  
+} catch (e) {
+  // Sentry not installed - use basic config
+  console.log('Note: Sentry package not found, continuing without Sentry integration');
 }
 
-const sentryOptions = {
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
-
-  // Transpiles SDK to be compatible with IE11 (increases bundle size)
-  transpileClientSDK: true,
-
-  // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-  tunnelRoute: "/monitoring",
-
-  // Hides source maps from generated client bundles
-  hideSourceMaps: true,
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors.
-  automaticVercelMonitors: true,
-}
-
-module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions, sentryOptions)
+module.exports = finalConfig;
